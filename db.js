1d72b48599923f465da6aab13f8a211d5eaf6532
diegoportes@gmail.com <diegoportes@gmail.comm>
2023-01-12 19:08:27 -0300

    Inclusão de linha a partir de outra máquina

--- db.js
+++ db.js
@@ -2,3 +2,4 @@ const Sequelize = require('sequelize')
 const sequelize = new Sequelize('crud','diegoportes','lancevak',{dialect: 'mysql', host: 'localhost', port: 3306})
 module.exports = sequelize
 /*Adição de comentário para ver se arquivo reflete.*/
+/*Mais um comentário para esse arquivo a partir de outra máquina */
