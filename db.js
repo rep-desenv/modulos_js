fba8c16d6ab5019c76028e4d5fe57c1a4b51b3a5
Lancevak@2022 <diegoportes@gmail.com>
2023-01-12 18:08:45 -0300

    Adição de comentário para ver se arquivo reflete.

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('crud','diegoportes','lancevak',{dialect: 'mysql', host: 'localhost', port: 3306})
 module.exports = sequelize
+/*Adição de comentário para ver se arquivo reflete.*/
